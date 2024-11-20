3be0b587ccf81dd1c570694406447d533efd6ada
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-11-20 12:57:48 +0100

    chore: move usage of M2MTokenService to SyfohelsenettproxyClient

--- src/main/kotlin/no/nav/tsm/sykinnapi/client/SyfohelsenettproxyClient.kt
+++ src/main/kotlin/no/nav/tsm/sykinnapi/client/SyfohelsenettproxyClient.kt
@@ -1,5 +1,6 @@
 package no.nav.tsm.sykinnapi.client
 
+import no.nav.tsm.sykinnapi.config.M2MTokenService
 import no.nav.tsm.sykinnapi.modell.syfohelsenettproxy.Behandler
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -12,7 +13,8 @@ import reactor.core.publisher.Mono
 @Component
 class SyfohelsenettproxyClient(
     syfohelsenettproxyM2mWebBuilder: WebClient.Builder,
-    @Value("\${syfohelsenettproxy.url}") syfohelsenettproxyBaseUrl: String
+    @Value("\${syfohelsenettproxy.url}") syfohelsenettproxyBaseUrl: String,
+    private val m2mTokenService: M2MTokenService
 ) {
     private val logger = LoggerFactory.getLogger(SyfohelsenettproxyClient::class.java)
 
@@ -28,6 +30,10 @@ class SyfohelsenettproxyClient(
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .header("Nav-CallId", sykmeldingId)
                 .header("hprNummer", behandlerHpr)
+                .header(
+                    HttpHeaders.AUTHORIZATION,
+                    "Bearer ${m2mTokenService.getM2MToken("syfohelsenettproxy-m2m")}"
+                )
                 .retrieve()
                 .onStatus({ status -> status.is4xxClientError || status.is5xxServerError }) {
                     response ->
