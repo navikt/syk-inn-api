dc23b0374cd49379d67edd8c30b0f684ccae7a9a
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-12-09 14:54:57 +0100

    chore: major refactor of application spring boot way

--- src/main/kotlin/no/nav/tsm/sykinnapi/SykInnApiApplication.kt
+++ src/main/kotlin/no/nav/tsm/sykinnapi/SykInnApiApplication.kt
@@ -1,6 +1,5 @@
 package no.nav.tsm.sykinnapi
 
-import no.nav.tsm.sykinnapi.util.Cluster.Companion.profiler
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -8,7 +7,5 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @SpringBootApplication @EnableWebSecurity class SykInnApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<SykInnApiApplication>(*args) {
-        setAdditionalProfiles(*profiler) // sett  profilerer automatisk
-    }
+    runApplication<SykInnApiApplication>(*args)
 }
