e7ad4a0d1e4e17f01a2b13d001f1aa37a7d88bbc
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-12-16 14:34:50 +0100

    chore: added in securityMatcher

--- src/main/kotlin/no/nav/tsm/sykinnapi/config/security/SecurityConfig.kt
+++ src/main/kotlin/no/nav/tsm/sykinnapi/config/security/SecurityConfig.kt
@@ -3,17 +3,16 @@ package no.nav.tsm.sykinnapi.config.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-@EnableWebSecurity
 class SecurityConfig {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http {
+            securityMatcher("/**")
             oauth2ResourceServer { jwt {} }
             oauth2Client {}
             authorizeHttpRequests {
