81264660d2b571dbf1a39e430c9733a2b847b205
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-12-16 14:45:03 +0100

    chore: removed securityMatcher

--- src/main/kotlin/no/nav/tsm/sykinnapi/SykInnApiApplication.kt
+++ src/main/kotlin/no/nav/tsm/sykinnapi/SykInnApiApplication.kt
@@ -2,9 +2,8 @@ package no.nav.tsm.sykinnapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
-@SpringBootApplication @EnableWebSecurity class SykInnApiApplication
+@SpringBootApplication class SykInnApiApplication
 
 fun main(args: Array<String>) {
     runApplication<SykInnApiApplication>(*args)
