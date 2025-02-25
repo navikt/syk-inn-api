e05790a56f33b3985bab41970c58f639ca4f20f1
Jørn-Are <ja.flaten91@gmail.com>
2025-02-25 09:37:12 +0100

    chore: cleaned up services and removed excess services

--- src/test/kotlin/no/nav/tsm/sykinnapi/service/sykmelding/SykmeldingServiceTest.kt
+++ src/test/kotlin/no/nav/tsm/sykinnapi/service/sykmelding/SykmeldingServiceTest.kt
@@ -3,16 +3,22 @@ package no.nav.tsm.sykinnapi.service.sykmelding
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.verify
 import no.nav.tsm.sykinnapi.config.kafka.SykmeldingOKProducer
 import no.nav.tsm.sykinnapi.modell.receivedSykmelding.Status
 import no.nav.tsm.sykinnapi.modell.receivedSykmelding.ValidationResult
 import no.nav.tsm.sykinnapi.modell.receivedSykmelding.toReceivedSykmeldingWithValidation
+import no.nav.tsm.sykinnapi.modell.syfohelsenettproxy.Behandler
 import no.nav.tsm.sykinnapi.modell.sykinn.Aktivitet
 import no.nav.tsm.sykinnapi.modell.sykinn.DiagnoseSystem
 import no.nav.tsm.sykinnapi.modell.sykinn.Hoveddiagnose
 import no.nav.tsm.sykinnapi.modell.sykinn.SykInnApiNySykmeldingPayload
 import no.nav.tsm.sykinnapi.modell.sykinn.Sykmelding
 import no.nav.tsm.sykinnapi.service.receivedSykmeldingMapper.ReceivedSykmeldingMapper
+import no.nav.tsm.sykinnapi.service.syfohelsenettproxy.SyfohelsenettproxyService
+import no.nav.tsm.sykinnapi.service.syfosmregister.SyfosmregisterService
+import no.nav.tsm.sykinnapi.service.syfosmregler.SyfosmreglerService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
@@ -22,15 +28,37 @@ import org.springframework.boot.test.autoconfigure.json.JsonTest
 @JsonTest
 class SykmeldingServiceTest {
 
-    @MockK lateinit var sykmeldingOKProducer: SykmeldingOKProducer
+    @MockK
+    lateinit var sykmeldingOKProducer: SykmeldingOKProducer
+
+    @MockK
+    lateinit var syfosmregisterService: SyfosmregisterService
+
+    @MockK
+    lateinit var syfohelsenettproxyService: SyfohelsenettproxyService
+
+    @MockK
+    lateinit var syfosmreglerService: SyfosmreglerService
+
+    @MockK
+    lateinit var receivedSykmeldingMapper: ReceivedSykmeldingMapper
 
     lateinit var sykmeldingService: SykmeldingService
 
-    @Autowired private lateinit var objectMapper: ObjectMapper
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
 
     @BeforeEach
     fun setup() {
-        sykmeldingService = SykmeldingService(sykmeldingOKProducer)
+        sykmeldingService =
+            SykmeldingService(
+                syfosmregisterService = syfosmregisterService,
+                sykmeldingOKProducer = sykmeldingOKProducer,
+                syfohelsenettproxyService = syfohelsenettproxyService,
+                syfosmreglerService = syfosmreglerService,
+                receivedSykmeldingMapper = receivedSykmeldingMapper,
+                objectMapper = objectMapper,
+            )
     }
 
     @Test
@@ -68,9 +96,58 @@ class SykmeldingServiceTest {
                     ValidationResult(
                         Status.OK,
                         emptyList(),
-                    )
+                    ),
                 )
 
         assertDoesNotThrow { sykmeldingService.sendToOkTopic(receivedSykmeldingWithValidation) }
+    }
+
+    @Test
+    fun shouldSendSykmeldingAndTriggerValidationAndProcessing() {
+
+        val fnr = "1344333"
+        val hpr = "123123"
+
+        val sykInnApiNySykmeldingPayload =
+            SykInnApiNySykmeldingPayload(
+                pasientFnr = "12345",
+                sykmelderHpr = "123123",
+                sykmelding =
+                    Sykmelding(
+                        hoveddiagnose =
+                            Hoveddiagnose(
+                                system = DiagnoseSystem.ICD10,
+                                code = "S017",
+                            ),
+                        aktivitet =
+                            Aktivitet.AktivitetIkkeMulig(
+                                fom = "2020-01-01",
+                                tom = "2020-01-02",
+                            ),
+                    ),
+            )
+        every { syfohelsenettproxyService.getBehandlerByHpr(any(), any()) } returns
+            Behandler(
+                godkjenninger = emptyList(),
+                fnr = fnr,
+                hprNummer = hpr,
+                fornavn = "Fornavn",
+                mellomnavn = null,
+                etternavn = "etternavn",
+            )
+        every { syfosmreglerService.validate(any()) } returns ValidationResult.OK
+        every { sykmeldingService.sendToOkTopic(any()) } returns Unit
+        every { receivedSykmeldingMapper.mapToReceivedSykmelding(any(), any(), any()) } returns
+            mockk()
+        every {
+            receivedSykmeldingMapper.mapToReceivedSykmeldingWithValidationResult(any(), any())
+        } returns mockk()
+
+        sykmeldingService.sendSykmelding(sykInnApiNySykmeldingPayload)
+        verify {
+            syfohelsenettproxyService.getBehandlerByHpr(any(), any())
+            syfosmreglerService.validate(any())
+            sykmeldingService.sendToOkTopic(any())
+        }
     }
 }
