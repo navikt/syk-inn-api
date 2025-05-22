a301dd7c548d32c25f4232de827f3dd8179a1853
Jørn-Are <ja.flaten91@gmail.com>
2025-05-22 14:52:23 +0200

    chore: added oauth2 client dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework:spring-webflux")
