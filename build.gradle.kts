4686d6b8e2dd4e801633337ca5122ed3e37dea8b
Karl J. Overå <k@rl.run>
2025-06-13 12:53:47 +0200

    chore: remove spring-boot-starter-oauth2-client because it angy

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework:spring-webflux")
