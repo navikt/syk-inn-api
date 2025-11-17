dced551f78d66baaadc18d8c8e1d2919eb6a5850
Jørn-Are <ja.flaten91@gmail.com>
2025-11-17 09:47:59 +0100

    chore: made deleteOldSykmeldinger job transactional

--- src/main/kotlin/no/nav/tsm/syk_inn_api/sykmelding/scheduled/DeleteOldSykmeldingerScheduledTask.kt
+++ src/main/kotlin/no/nav/tsm/syk_inn_api/sykmelding/scheduled/DeleteOldSykmeldingerScheduledTask.kt
@@ -4,6 +4,7 @@ import no.nav.tsm.syk_inn_api.sykmelding.persistence.SykmeldingPersistenceServic
 import no.nav.tsm.syk_inn_api.utils.logger
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 const val DAYS_OLD_SYKMELDING = 365L
 
@@ -12,6 +13,7 @@ class ScheduledTasks(private val sykmeldingPersistenceService: SykmeldingPersist
     private val logger = logger()
 
     @Scheduled(cron = "0 0 2 * * ?") // Runs every day at 2 AM
+    @Transactional
     fun deleteOldSykmeldinger() {
         logger.info("Starting task to delete old sykmeldinger older than $DAYS_OLD_SYKMELDING days")
 
