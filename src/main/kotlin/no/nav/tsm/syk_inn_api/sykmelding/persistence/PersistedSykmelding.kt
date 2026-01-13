6028fc6e3236a0f36736bd8b7770661845deec65
Karl J. Overå <k@rl.run>
2026-01-13 11:57:55 +0100

    feat: add support for annen lovfesten fraværsgrunn
    Co-authored-by: Andreas <danduras@gmail.com>

--- src/main/kotlin/no/nav/tsm/syk_inn_api/sykmelding/persistence/PersistedSykmelding.kt
+++ src/main/kotlin/no/nav/tsm/syk_inn_api/sykmelding/persistence/PersistedSykmelding.kt
@@ -9,6 +9,7 @@ import java.time.OffsetDateTime
 import no.nav.tsm.syk_inn_api.common.DiagnoseSystem
 import no.nav.tsm.syk_inn_api.person.Navn
 import no.nav.tsm.syk_inn_api.sykmelding.response.SykInnArbeidsrelatertArsakType
+import no.nav.tsm.sykmelding.input.core.model.AnnenFravarsgrunn
 
 data class PersistedSykmelding(
     val sykmeldingId: String,
@@ -24,6 +25,7 @@ data class PersistedSykmelding(
     val arbeidsgiver: PersistedSykmeldingArbeidsgiver?,
     val tilbakedatering: PersistedSykmeldingTilbakedatering?,
     val utdypendeSporsmal: PersistedSykmeldingUtdypendeSporsmal?,
+    val annenFravarsgrunn: AnnenFravarsgrunn?,
     val regelResultat: PersistedSykmeldingRuleResult,
 )
 
