634e24ea7b68107d55f6eb7a3b39590899ca34d0
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2026-01-21 14:46:04 +0100

    chore: use explicit starter dependencies, remove old

--- src/test/kotlin/no/nav/tsm/syk_inn_api/pdf/PdfControllerTest.kt
+++ src/test/kotlin/no/nav/tsm/syk_inn_api/pdf/PdfControllerTest.kt
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest
 import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(PdfController::class)
+@WithMockUser
 class PdfControllerTest {
 
     @Autowired private lateinit var mockMvc: MockMvc
