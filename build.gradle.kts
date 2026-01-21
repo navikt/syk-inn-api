a663e8135b55d7e272e593b02f61f5f438b91e0a
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2026-01-21 08:31:52 +0100

    chore: upgrade gradle to 4.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ plugins {
     kotlin("jvm") version "2.2.21"
     kotlin("plugin.spring") version "2.2.21"
     kotlin("plugin.jpa") version "2.2.21"
-    id("org.springframework.boot") version "3.5.9"
+    id("org.springframework.boot") version "4.0.1"
 
     // Other plugins
     id("io.spring.dependency-management") version "1.1.7"
