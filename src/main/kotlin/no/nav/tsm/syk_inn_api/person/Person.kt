ee7de64ac0d4a9a1647b4733e59f9428d6de803b
Karl J. Overå <k@rl.run>
2026-01-27 10:31:18 +0100

    chore: remove PDF generation
    Co-authored-by: Andreas <danduras@gmail.com>
    Co-authored-by: Jørn-Are Flaten <ja.flaten91@gmail.com>
    Co-authored-by: Leo-Andreas Ervik <leo.ervik@gmail.com>

--- src/main/kotlin/no/nav/tsm/syk_inn_api/person/Person.kt
+++ src/main/kotlin/no/nav/tsm/syk_inn_api/person/Person.kt
@@ -8,9 +8,6 @@ data class Person(
     val fodselsdato: LocalDate,
 )
 
-fun Person.displayName(): String =
-    "${navn.fornavn}${if (navn.mellomnavn != null) " ${navn.mellomnavn} " else " "}${navn.etternavn}"
-
 data class Navn(
     val fornavn: String,
     val mellomnavn: String?,
