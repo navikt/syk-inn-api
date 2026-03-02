eb85fa55f0a40e6c511b7a17a8ebe4c649a5f56e
Karl J. Overå <k@rl.run>
2026-03-02 18:16:11 +0100

    chore: deploy to dev-gcp as its own app
    Co-authored-by: Andreas <danduras@gmail.com>

--- src/main/kotlin/core/db/Postgres.kt
+++ src/main/kotlin/core/db/Postgres.kt
@@ -3,6 +3,7 @@ package no.nav.tsm.core.db
 import io.ktor.server.config.ApplicationConfig
 import java.sql.Connection
 import java.sql.DriverManager
+import java.util.Properties
 
 
 fun connectToPostgres(config: ApplicationConfig): Connection {
@@ -11,21 +12,11 @@ fun connectToPostgres(config: ApplicationConfig): Connection {
     val password = config.property("postgres.password").getString()
 
     runFlywayMigrations(url, user, password)
-    // fikse envs
-    // koble til postgres
-    // returnere connection til DI
 
-    /**
-     * String url = "jdbc:postgresql://localhost/test";
-     * Properties props = new Properties();
-     * props.setProperty("user", "fred");
-     * props.setProperty("password", "secret");
-     * props.setProperty("ssl", "true");
-     * Connection conn = DriverManager.getConnection(url, props);
-     *
-     * String url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
-     * Connection conn = DriverManager.getConnection(url);
-     */
+    val properties = Properties().apply {
+        setProperty("user", user)
+        setProperty("password", password)
+    }
 
-    return DriverManager.getConnection("jdbc:postgresql://localhost:7969/syk-inn-api?user=postgres&password=password")
+    return DriverManager.getConnection(url, properties)
 }
