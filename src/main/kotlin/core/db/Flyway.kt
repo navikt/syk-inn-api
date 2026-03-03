fef46f17ac14670de1835a66b9eb4658a23bac1a
Karl J. Overå <k@rl.run>
2026-03-03 09:18:52 +0100

    chore: always be cleanin'

--- src/main/kotlin/core/db/Flyway.kt
+++ src/main/kotlin/core/db/Flyway.kt
@@ -4,7 +4,11 @@ import org.flywaydb.core.Flyway
 
 fun runFlywayMigrations(url: String, user: String, password: String) {
     val flyway =
-        Flyway.configure().dataSource(url, user, password).locations("db/migrations").load()
+        Flyway.configure()
+            .cleanDisabled(false)
+            .dataSource(url, user, password).locations("db/migrations").load()
 
+    // In this development period, we'll edit the first changeset and always nuke DB
+    flyway.clean()
     flyway.migrate()
 }
