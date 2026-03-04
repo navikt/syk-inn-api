a7f69ade2c433162709a94e530c962d57721ab9f
Karl J. Overå <k@rl.run>
2026-03-04 22:55:59 +0100

    chore: add retry mechanism to external HttpClient

--- src/main/kotlin/modules/external/ExternalDependencies.kt
+++ src/main/kotlin/modules/external/ExternalDependencies.kt
@@ -1,6 +1,7 @@
 package modules.external
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.Application
@@ -9,17 +10,19 @@ import modules.external.clients.pdl.PdlClient
 import modules.external.clients.texas.TexasClient
 
 fun Application.configureExternalDependencies() {
+    val baseHttpClient: HttpClient by dependencies
+
     dependencies {
-        key<HttpClient>("ExternalHttpClient") { createExternalApiHttpClient() }
+        provide("RetryHttpClient") { createExternalApiHttpClient(baseHttpClient) }
         provide(TexasClient::class)
         provide(PdlClient::class)
     }
 }
 
-private fun createExternalApiHttpClient() {
-    val baseHttp = HttpClient()
-
-    val retryHttp = baseHttp.config { install(ContentNegotiation) { jackson() } }
-
-    HttpClient { install(ContentNegotiation) { jackson() } }
+private fun createExternalApiHttpClient(baseHttpClient: HttpClient) = HttpClient {
+    install(ContentNegotiation) { jackson() }
+    install(HttpRequestRetry) {
+        retryOnServerErrors(maxRetries = 5)
+        exponentialDelay()
+    }
 }
