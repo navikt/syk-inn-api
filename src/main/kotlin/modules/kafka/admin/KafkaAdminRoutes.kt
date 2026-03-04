d0f59770ec089bc9b85ffabbe0cc5388e23de17e
Karl J. Overå <k@rl.run>
2026-03-04 13:39:59 +0100

    chore: add status to kafka admin routes

--- src/main/kotlin/modules/kafka/admin/KafkaAdminRoutes.kt
+++ src/main/kotlin/modules/kafka/admin/KafkaAdminRoutes.kt
@@ -1,12 +1,16 @@
 package no.nav.tsm.modules.kafka.admin
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.plugins.di.dependencies
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import no.nav.tsm.core.logger
 import no.nav.tsm.modules.kafka.consume.SykmeldingConsumerJobManager
+import kotlin.collections.mapOf
 
 fun Application.configureKafkaAdminRoutes() {
     val logger = logger()
@@ -16,15 +20,26 @@ fun Application.configureKafkaAdminRoutes() {
         // TODO: Obo auth token, verifiser team medlem (group?)
 
         route("/internal/kafka/consumer") {
+            get("/status") {
+                call.respond(HttpStatusCode.OK, mapOf("status" to service.status().name))
+            }
             post("/start") {
                 logger.info("Got admin command to START consumer from USER (TODO)")
 
-                service.start()
+                val started = service.start()
+                call.respond(
+                    if (started) HttpStatusCode.Accepted else HttpStatusCode.Conflict,
+                    mapOf("status" to service.status().name)
+                )
             }
             post("/stop") {
                 logger.info("Got admin command to STOP consumer from USER (TODO)")
 
-                service.stop()
+                val stopped = service.stop()
+                call.respond(
+                    if (stopped) HttpStatusCode.Accepted else HttpStatusCode.Conflict,
+                    mapOf("status" to service.status().name)
+                )
             }
         }
     }
