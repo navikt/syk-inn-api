53e39e6b5b3abe9bd262ae9681d688ba00590e8e
Karl J. Overå <k@rl.run>
2026-03-05 19:17:46 +0100

    chore: reduce log level for jobs scheduler

--- src/main/kotlin/plugins/Openapi.kt
+++ src/main/kotlin/plugins/Openapi.kt
@@ -7,6 +7,12 @@ import io.ktor.server.routing.*
 
 fun Application.configureOpenAPI() {
     routing {
-        openAPI(path = "openapi") { info = OpenApiInfo(title = "Syk Inn API", version = "1.0.0") }
+        openAPI(path = "openapi") {
+            info = OpenApiInfo(
+                title = "Syk Inn API",
+                version = "1.0.0",
+                description = "Syk inn API is a direct API for syk-inn only accessible through service discovery in the nais k8s cluster."
+            )
+        }
     }
 }
