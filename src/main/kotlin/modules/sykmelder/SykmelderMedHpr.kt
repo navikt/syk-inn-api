5cb2db7b818675417ce34bcdd22ddb42133df804
Karl J. Overå <k@rl.run>
2026-03-05 14:15:59 +0100

    chore: implement HprClient
    Co-authored-by: Andreas <danduras@gmail.com>
    Co-authored-by: Jørn-Are Flaten <ja.flaten91@gmail.com>

---
+++ src/main/kotlin/modules/sykmelder/SykmelderMedHpr.kt
@@ -0,0 +1,11 @@
+package modules.sykmelder
+
+data class SykmelderMedHpr(
+    val ident: String,
+    val hprNummer: String,
+    val fornavn: String,
+    val mellomnavn: String?,
+    val etternavn: String,
+    // TODO("Legg på liste med godkjenninger")
+
+)
