7843f1b9f5bee7731bff6018b5f63daf3bfe72c4
Andreas <danduras@gmail.com>
2026-03-05 20:22:09 +0100

    chore: update status when job status changes

--- src/main/kotlin/modules/jobs/JobSceduling.kt
+++ src/main/kotlin/modules/jobs/JobSceduling.kt
@@ -4,9 +4,10 @@ import core.logger
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationStarted
 import io.ktor.server.application.ApplicationStarting
-import io.ktor.server.application.ApplicationStopping
 import io.ktor.server.plugins.di.dependencies
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import modules.jobs.service.JobScheduler
 
 fun Application.configureJobSceduling() {
@@ -19,17 +20,19 @@ fun Application.configureJobSceduling() {
             jobScheduler.setup()
         }
     }
-    monitor.subscribe(ApplicationStopping) {
-        launch {
-            log.info("Job sceduling stopped")
-            jobScheduler.stop()
-        }
-    }
 
     monitor.subscribe(ApplicationStarted) {
         launch {
-            log.info("Job sceduling started")
-            jobScheduler.start()
+            try {
+                log.info("Job sceduling started")
+                jobScheduler.start()
+            } finally {
+                withContext(NonCancellable) {
+                    log.info("Job sceduling stopping")
+                    jobScheduler.stop()
+                    log.info("Job sceduling stopped")
+                }
+            }
         }
     }
 }
--- src/main/kotlin/plugins/Openapi.kt
+++ src/main/kotlin/plugins/Openapi.kt
@@ -8,11 +8,13 @@ import io.ktor.server.routing.*
 fun Application.configureOpenAPI() {
     routing {
         openAPI(path = "openapi") {
-            info = OpenApiInfo(
-                title = "Syk Inn API",
-                version = "1.0.0",
-                description = "Syk inn API is a direct API for syk-inn only accessible through service discovery in the nais k8s cluster."
-            )
+            info =
+                OpenApiInfo(
+                    title = "Syk Inn API",
+                    version = "1.0.0",
+                    description =
+                        "Syk inn API is a direct API for syk-inn only accessible through service discovery in the nais k8s cluster.",
+                )
         }
     }
 }
