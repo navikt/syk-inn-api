b7887ef354a77a4e8ac10dc03a4a2d638a78e76f
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2026-05-07 10:46:15 +0200

    fix: remove unused logger

--- src/main/kotlin/no/nav/tsm/core/otel/Otel.kt
+++ src/main/kotlin/no/nav/tsm/core/otel/Otel.kt
@@ -2,22 +2,19 @@ package no.nav.tsm.core.otel
 
 import io.opentelemetry.api.trace.Span
 import io.opentelemetry.api.trace.StatusCode
-import no.nav.tsm.core.logger
-
-private val log = logger()
 
 fun failSpan(span: Span, exception: Throwable): Throwable {
-    span.setStatus(StatusCode.ERROR)
-    span.recordException(exception)
-    return exception
+  span.setStatus(StatusCode.ERROR)
+  span.recordException(exception)
+  return exception
 }
 
 fun Throwable.failSpan(): Throwable {
-    val span = Span.current()
+  val span = Span.current()
 
-    return failSpan(span, this)
+  return failSpan(span, this)
 }
 
 fun Throwable.failSpan(span: Span): Throwable {
-    return failSpan(span, this)
+  return failSpan(span, this)
 }
