6a653b97174b743889ef537dba10555bddab5a6f
Karl J. Overå <k@rl.run>
2026-08-05 13:29:23 +0200

    refactor: use NaisMonitoring

--- src/main/kotlin/no/nav/tsm/plugins/Monitoring.kt
+++ src/main/kotlin/no/nav/tsm/plugins/Monitoring.kt
@@ -1,30 +1,12 @@
 package no.nav.tsm.plugins
 
-import dev.hayden.KHealth
-import io.ktor.http.HttpHeaders
 import io.ktor.server.application.*
-import io.ktor.server.engine.ShutDownUrl
-import io.ktor.server.metrics.micrometer.MicrometerMetrics
-import io.ktor.server.plugins.callid.CallId
-import io.ktor.server.plugins.callid.callIdMdc
-import io.ktor.server.plugins.calllogging.CallLogging
-import io.ktor.server.response.respond
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
-import io.micrometer.prometheusmetrics.PrometheusConfig
-import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import no.nav.tsm.core.db.dbQuery
-import org.slf4j.event.Level
+import no.nav.tsm.ktor.nais.NaisMonitoring
 
 fun Application.configureMonitoring() {
-    configureMicrometer()
-
-    install(KHealth) {
-        healthChecks { healthCheckPath = "/internal/health/alive" }
-
-        readyChecks {
-            readyCheckPath = "/internal/health/ready"
-
+    install(NaisMonitoring) {
+        ready {
             check("database ready") {
                 try {
                     dbQuery { exec("SELECT 1") }
@@ -35,23 +17,4 @@ fun Application.configureMonitoring() {
             }
         }
     }
-    install(ShutDownUrl.ApplicationCallPlugin) {
-        shutDownUrl = "/internal/shutdown"
-        exitCodeSupplier = { 0 }
-    }
-    install(CallId) {
-        retrieveFromHeader("Traceparent")
-        retrieveFromHeader(HttpHeaders.XRequestId)
-    }
-    install(CallLogging) {
-        level = Level.DEBUG
-        callIdMdc("call-id")
-    }
-}
-
-private fun Application.configureMicrometer() {
-    val appRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
-
-    install(MicrometerMetrics) { registry = appRegistry }
-    routing { get("/internal/metrics") { call.respond(appRegistry.scrape()) } }
 }
