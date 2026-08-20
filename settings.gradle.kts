92eabff1c3ebd95685c938e820e7502e91655c76
Karl J. Overå <k@rl.run>
2026-08-20 14:05:50 +0200

    automated: upgrade tsm-ktor libs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "syk-inn-api"
 
 val ktorVersion = "3.5.2"
-val tsmKtorVersion = "1.2.0"
+val tsmKtorVersion = "1.2.5"
 val exposedVersion = "1.4.0"
 
 dependencyResolutionManagement {
