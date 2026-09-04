a63b2a1c07f43f60f0b7b3548f9f26bda697f57e
Andreas <danduras@gmail.com>
2026-09-04 15:30:42 +0200

    fix: use correct schema

--- src/main/kotlin/no/nav/tsm/plugins/Database.kt
+++ src/main/kotlin/no/nav/tsm/plugins/Database.kt
@@ -4,6 +4,8 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.di.*
 import no.nav.tsm.core.Environment
 import no.nav.tsm.core.db.getFlyway
+import org.jetbrains.exposed.v1.core.DatabaseConfig
+import org.jetbrains.exposed.v1.core.Schema
 import org.jetbrains.exposed.v1.jdbc.Database
 
 fun Application.configureDatabase() {
@@ -15,5 +17,6 @@ fun Application.configureDatabase() {
         url = env.postgres.jdbc,
         user = env.postgres.username,
         password = env.postgres.password,
+        databaseConfig = DatabaseConfig.invoke { defaultSchema = Schema(env.postgres.schema) },
     )
 }
